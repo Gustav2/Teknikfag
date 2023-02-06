c1f2916ff42bbaf05e7e3b84fc8c3d8235f186cb
Gustav <gustav@gustavnybro.com>
2023-02-06 19:23:19 +0100

    added code for maltgrabber

--- Servo/Servo/main.c
+++ Servo/Servo/main.c
@@ -10,11 +10,26 @@ int main(void) {
 	
 	initServo(900); // 1150 for 270
 	
+	int isOpen = 0;
 	counterClockwise();
-	_delay_ms(3000);
-	clockwise();
-
-
+	
+	while (1)
+	{
+		if (PINB & (1 << 0))
+		{
+			if (isOpen)
+			{
+				turnDegrees(0);
+				isOpen = 0;
+				_delay_ms(1000);
+			} 
+			else {
+				turnDegrees(90);
+				isOpen = 1;
+				_delay_ms(1000);
+			}	
+		}
+	}
 	
 	return 0;
 }
